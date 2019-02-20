66992950341fc0982c0e7ea4136365f32f1d48e7
Sonia Dumitru <sonya_dumitru@yahoo.com>
2019-02-20 05:57:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,7 +6,7 @@
 #end
 
 def reverse_each_word(string)
-      array = string.split(" ")
+      string = string.split(" ")
       new_sentence = []
     string.each do |word|
       string = word.reverse
